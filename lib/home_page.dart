ed825eeae9739608b680b672eb27998a33cc987e
Sanyam Goel <sanyam.2024csit1147@kiet.edu>
2023-06-09 00:48:40 +0530

    Done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/login_page.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,27 +12,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isDarkMode = false;
   PickedFile? _image;
-  String? _location;
+  bool _showConfirmation = false;
 
   Future _getImageFromCamera() async {
     final image = await ImagePicker().pickImage(source: ImageSource.camera);
     if (image != null) {
       setState(() {
         _image = PickedFile(image.path);
+        _showConfirmation = true;
       });
     }
-  }
-
-  Future _getCurrentLocation() async {
-    final position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    setState(() {
-      _location =
-          'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
-    });
   }
 
   void _logout(BuildContext context) {
@@ -47,67 +36,103 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final lightTheme = ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Colors.blue, // Customize your light mode theme color
-      // Add other light mode theme configurations
-    );
+  final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue, // Customize your light mode theme color
+    // Add other light mode theme configurations
+  );
 
-    final darkTheme = ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.deepPurple, // Customize your dark mode theme color
-      // Add other dark mode theme configurations
-    );
-
-    return Theme(
-      data: isDarkMode ? darkTheme : lightTheme,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Camera & Location Demo'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Switch(
-              value: isDarkMode,
-              onChanged: (value) {
-                setState(() {
-                  isDarkMode = value;
-                });
-              },
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                _getImageFromCamera();
-                _getCurrentLocation();
-              },
-              child: const Text('Check-in'),
-            ),
-            const SizedBox(height: 16.0),
-            if (_image != null)
-              Image.file(
-                File(_image!.path),
-                width: 200,
-                height: 200,
-              ),
-            if (_location != null)
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  _location!,
-                  style: const TextStyle(fontSize: 16.0),
+  return Theme(
+    data: lightTheme,
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Happy Animal'),
+      ),
+      body: Stack(
+        children: [
+          if (_image != null)
+            Positioned.fill(
+              child: Center(
+                child: Image.asset(
+                  "assets/images/plate4.jpg", // Image of eating plate
+                  fit: BoxFit.cover,
                 ),
               ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.logout),
-          onPressed: () {
-            _logout(context);
-          },
-        ),
+            ),
+          if (_image != null)
+            Positioned.fill(
+              child: Center(
+                child: ClipOval(
+                  child: Image.file(
+                    File(_image!.path),
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          if (_image != null)
+            Positioned(
+              top: kToolbarHeight - 50.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Image.asset(
+                  "assets/images/animal1.jpg", // Image of the animal
+                  width: 400,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Column(
+              children: [
+                const SizedBox(height: 16.0),
+                GestureDetector(
+                  onTap: () {
+                    _getImageFromCamera();
+                  },
+                  child: const DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Icon(
+                        Icons.camera_alt,
+                        size: 64,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  _showConfirmation ? 'Will you eat this?' : 'Click your meal',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+              ],
+            ),
+          ),
+        ],
       ),
-    );
-  }
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.logout),
+        onPressed: () {
+          _logout(context);
+        },
+      ),
+    ),
+  );
+ }
 }
